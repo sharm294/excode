41dee81f492537163d0010462fc9d095432a4f62
Varun Sharma <vsharma.ix@gmail.com>
2019-12-10 18:38:19 -0800

    Switch to using strings for cmd args

--- excode/main.py
+++ excode/main.py
@@ -41,7 +41,8 @@ def change_endings(infile):
     WINDOWS_LINE_ENDING = "\r\n"
     UNIX_LINE_ENDING = "\n"
 
-    content = infile.read()
+    with open(infile, "r") as f:
+        content = f.read()
     if platform.system() == "Linux" or platform.system() == "Darwin":
         target_ending = UNIX_LINE_ENDING
         bad_ending = WINDOWS_LINE_ENDING
@@ -52,67 +53,68 @@ def change_endings(infile):
         raise ValueError("Unsupported platform detected: %s" % platform.system())
     content = content.replace(WINDOWS_LINE_ENDING, UNIX_LINE_ENDING)
 
-    outfile = infile.name + ".tmp"
-    with open(outfile, "w+") as f:
+    outfile = infile + ".tmp"
+    with open(outfile, "w") as f:
         f.write(content)
 
-    return open(outfile)
+    return outfile
 
 
 # inspired from https://github.com/PyCQA/pylint/blob/master/pylint/utils/pragma_parser.py
 MARKDOWN_EXCODE_COMMENT = r"(\s*\[\/\/\]: \# \(.*?\bexcode:\s*(.*=.*))\)"
 
 
-def extract(f, filter_str=None):
+def extract(infile, filter_str=None):
 
-    f = change_endings(f)
+    infile_2 = change_endings(infile)
 
     code_blocks = []
 
-    line = f.readline()
-    options = re.match(MARKDOWN_EXCODE_COMMENT, line)
-    if options:
-        s = str(options.group(2))
-        extracted = dict(item.split("=") for item in s.split(","))
-        extracted["code_blocks"] = []
-    else:
-        return {"code_blocks": []}
-
-    while True:
+    with open(infile_2, "r") as f:
         line = f.readline()
-        if not line:
-            # EOF
-            break
+        options = re.match(MARKDOWN_EXCODE_COMMENT, line)
+        if options:
+            s = str(options.group(2))
+            extracted = dict(item.split("=") for item in s.split(","))
+            extracted["code_blocks"] = []
+        else:
+            return {"code_blocks": []}
 
-        out = re.match("[^`]*```(.*)$", line)
-        if out:
-            block_metadata = out.group(1).strip()
-            if filter_str and filter_str.strip() not in block_metadata:
-                continue
-            if "excode" in block_metadata:
-                metadata_str = block_metadata.split("excode:")[1]
-                metadata = dict(
-                    item.strip().split("=") for item in metadata_str.split(",")
-                )
-            else:
-                metadata = {}
-            metadata["code"] = [f.readline()]
-            code_block = []
-            while re.search("```", metadata["code"][-1]) is None:
-                metadata["code"].append(f.readline())
-            metadata["code"] = "".join(metadata["code"][:-1])
+        while True:
+            line = f.readline()
+            if not line:
+                # EOF
+                break
 
-            if "attach" in metadata:
-                if metadata["attach"] == "prev":
-                    extracted["code_blocks"][-1]["code"] += metadata["code"]
-                else:
-                    extracted["code_blocks"][int(metadata["attach"])]["code"].join(
-                        metadata["code"]
+            out = re.match("[^`]*```(.*)$", line)
+            if out:
+                block_metadata = out.group(1).strip()
+                if filter_str and filter_str.strip() not in block_metadata:
+                    continue
+                if "excode" in block_metadata:
+                    metadata_str = block_metadata.split("excode:")[1]
+                    metadata = dict(
+                        item.strip().split("=") for item in metadata_str.split(",")
                     )
-            else:
-                extracted["code_blocks"].append(metadata)
+                else:
+                    metadata = {}
+                metadata["code"] = [f.readline()]
+                code_block = []
+                while re.search("```", metadata["code"][-1]) is None:
+                    metadata["code"].append(f.readline())
+                metadata["code"] = "".join(metadata["code"][:-1])
 
-    os.remove(f.name)  # remove temporary file created by change_endings()
+                if "attach" in metadata:
+                    if metadata["attach"] == "prev":
+                        extracted["code_blocks"][-1]["code"] += metadata["code"]
+                    else:
+                        extracted["code_blocks"][int(metadata["attach"])]["code"].join(
+                            metadata["code"]
+                        )
+                else:
+                    extracted["code_blocks"].append(metadata)
+
+    os.remove(infile_2)  # remove temporary file created by change_endings()
 
     return extracted
 
@@ -127,7 +129,7 @@ def write_python_function(code_blocks, prefix):
     return fun_strings
 
 
-def write_python(f, code_blocks, prefix):
+def write_python(infile, code_blocks, prefix):
     # We'd like to put all code blocks in one file, each in separate test*()
     # functions (for them to be picked up by pytest, for example), but
     # asterisk imports are forbidden in subfunctions. Hence, parse for those
@@ -145,18 +147,13 @@ def write_python(f, code_blocks, prefix):
     # make list unique
     asterisk_imports = list(set(asterisk_imports))
 
-    if asterisk_imports:
-        f.write("\n".join(asterisk_imports))
-        f.write("\n\n")
+    with open(infile, "w") as f:
+        if asterisk_imports:
+            f.write("\n".join(asterisk_imports))
+            f.write("\n\n")
 
-    # fun_strings = []
-    # for k, code_block in enumerate(clean_code_blocks):
-    #     fun_strings.append("")
-    #     fun_strings[-1] += "def {}{}():\n".format(prefix, k)
-    #     fun_strings[-1] += indent(code_block, 4)
-    #     fun_strings[-1] += "    return\n"
-    fun_strings = write_python_function(clean_code_blocks, prefix)
-    f.write("\n\n".join(fun_strings))
+        fun_strings = write_python_function(clean_code_blocks, prefix)
+        f.write("\n\n".join(fun_strings))
 
 
 def write_bash_switch(num, prefix):
@@ -187,14 +184,14 @@ def write_bash_wrapper(filepath, num, prefix):
         )
     functions = write_python_function(code_blocks, prefix)
     fun_strings.extend(functions)
-    python_filepath = filepath.replace(".sh", ".py")
+    python_filepath = str(filepath).replace(".sh", ".py")
     with open(python_filepath, "w") as f:
         f.write("\n\n".join(fun_strings))
 
     return
 
 
-def write_bash(f, code_blocks, prefix):
+def write_bash(infile, code_blocks, prefix):
     fun_strings = []
     for k, code_block in enumerate(code_blocks):
         fun_strings.append("")
@@ -205,11 +202,11 @@ def write_bash(f, code_blocks, prefix):
     fun_strings.extend(write_bash_switch(len(code_blocks), prefix))
     # fun_strings.append("")
     # fun_strings[-1] += "export NUM_TESTS=%s\n" % str(len(code_blocks))
-    f.write("\n".join(fun_strings))
+    with open(infile, "w") as f:
+        f.write("\n".join(fun_strings))
 
-    filename = f.name
-    os.chmod(filename, 0o755)
-    write_bash_wrapper(filename, len(code_blocks), prefix)
+    os.chmod(infile, 0o755)
+    write_bash_wrapper(infile, len(code_blocks), prefix)
 
 
 def write(f, extracted, prefix="test_"):
--- test/test_excode.py
+++ test/test_excode.py
@@ -13,9 +13,8 @@ def get_out_file(root_path, filename):
     return
 
 
-def test_plain(get_file):
-    with open(get_file.join("markdown/test_plain.md")) as inp:
-        extracted = excode.extract(inp)
+def test_plain(get_file, get_out_dir):
+    extracted = excode.extract(get_file.join("markdown/test_plain.md"))
     assert len(extracted["code_blocks"]) == 1
     target_value = textwrap.dedent(
         """\
@@ -26,8 +25,9 @@ def test_plain(get_file):
     """
     )
     assert extracted["code_blocks"][0]["code"] == target_value
-    out = io.StringIO()
-    excode.write(out, extracted)
+
+    filepath = get_out_dir.join("/test_plain.sh")
+    excode.write(filepath, extracted)
     target_value = textwrap.dedent(
         """\
         def test_0():
@@ -36,28 +36,28 @@ def test_plain(get_file):
             add = 4 + 5
             print(add)
             return
-    """
+        """
     )
-    assert out.getvalue() == target_value
+    with open(filepath, "r") as out:
+        assert "".join(out.readlines()) == target_value
     return
 
 
 def test_filter(get_file):
-    with open(get_file.join("markdown/test_filter.md")) as inp:
-        extracted = excode.extract(inp, filter_str="python")
+    extracted = excode.extract(
+        get_file.join("markdown/test_filter.md"), filter_str="python"
+    )
     assert len(extracted["code_blocks"]) == 1
     assert extracted["code_blocks"][0]["code"] == "1 + 2 + 3\n"
-    return
 
 
 def test_shell(get_file, get_out_dir):
-    with open(get_file.join("markdown/test_shell.md")) as inp:
-        extracted = excode.extract(inp)
+    extracted = excode.extract(get_file.join("markdown/test_shell.md"))
     assert len(extracted["code_blocks"]) == 1
     assert extracted["code_blocks"][0]["code"] == "export FOO=bar\n"
 
-    out = open(get_out_dir.join("/test_shell.sh"), "w")
-    excode.write(out, extracted)
+    filepath = get_out_dir.join("/test_shell.sh")
+    excode.write(filepath, extracted)
 
     target_value = textwrap.dedent(
         """\
@@ -68,11 +68,10 @@ def test_shell(get_file, get_out_dir):
         if [[ $1 == 0 ]]; then
             test_0
         fi
-    """
+        """
     )
-    filepath = get_out_dir.join("/test_shell.sh")
-    out = open(filepath, "r")
-    assert "".join(out.readlines()) == target_value
+    with open(filepath, "r") as out:
+        assert "".join(out.readlines()) == target_value
 
     target_value = textwrap.dedent(
         f"""\
@@ -81,8 +80,7 @@ def test_shell(get_file, get_out_dir):
         def test_0():
             result = subprocess.run(["{filepath} 0"], stdout=subprocess.PIPE, shell=True)
             return
-    """
+        """
     )
-    out = open(str(filepath).replace(".sh", ".py"), "r")
-    assert "".join(out.readlines()) == target_value
-    return
+    with open(str(filepath).replace(".sh", ".py"), "r") as out:
+        assert "".join(out.readlines()) == target_value
