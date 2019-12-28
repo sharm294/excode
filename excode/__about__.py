b1304979bf45af01cfb1a4781a0b5c8ab51912ab
Varun Sharma <vsharma.ix@gmail.com>
2019-12-28 18:07:31 -0500

    Bump version

--- excode/__about__.py
+++ excode/__about__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-__version__ = "0.5.0"
+__version__ = "0.5.1"
 __author__ = u"Nico Schlömer and Varun Sharma"
 __author_email__ = "nico.schloemer@gmail.com"
 __status__ = "Development Status :: 5 - Beta"
